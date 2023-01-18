0ae91f5192d5bb659d34da20e17718687248e7b4
sarthak04agarwal <sarthak04agarwal@gmail.com>
2023-01-18 03:44:31 -0700

    Create draw.py

---
+++ draw.py
@@ -0,0 +1,43 @@
+
+import pygame
+from math import pi  
+
+# Constructor for pygame
+pygame.init()  
+
+# Setting the width and the height of the screen
+screen_Size = [500, 500]
+
+# Setting the screen size 
+screen = pygame.display.set_mode(screen_Size)    
+   
+# loop variable is using as flag     
+loop = False      
+while not loop:      
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:      
+            loop = True    
+
+    # Turns the whole screen into the color black   
+    screen.fill((0, 0, 0))    
+    
+    # Draw on the screen a big circle
+    #pygame.draw.circle(screen, 0, 250, 100)
+    pygame.draw.circle(screen, (0, 255, 0), (250, 250), 200)
+    pygame.draw.circle(screen, (255, 0, 0), (350, 200), 40)
+    pygame.draw.circle(screen, (255, 0, 0), (150, 200), 40)
+    #pygame.draw.triangle
+    #pygame.draw.polygon(screen, (0, 0, 0), [[100, 100], [250, 500], [200, 200]], 5)
+    #pygame.draw.ellipse(screen, (0, 0, 255), (250, 300), 20)
+    pygame.draw.ellipse(screen, (0, 0, 255), [225, 250, 50, 60])
+
+    pygame.draw.circle(screen, "black", [250, 350], 50, 10, draw_bottom_left=True)
+    pygame.draw.circle(screen, "black", [250, 350], 50, 10, draw_bottom_right=True)
+
+
+    
+    # This is required to project the image    
+    pygame.display.flip()    
+    
+# Close the pop-up window when the user clicks on exit
+pygame.quit()    
